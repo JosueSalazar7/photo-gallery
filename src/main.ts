eb53aa3b72ba1c428a22f5de6586b578a91977b0
JosueSalazar7 <josue.salazar@epn.edu.ec>
2023-11-15 15:42:40 -0500

    hoja de vida

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-
+import {} from '@ionic/pwa-elements/loader'
 if (environment.production) {
   enableProdMode();
 }
